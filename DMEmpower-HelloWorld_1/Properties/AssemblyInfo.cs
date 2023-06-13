81e03a725ea6ccb23600b9908cd0d09b21ef75f5
611805919 <611805919@BT-023163620957>
2023-06-13 14:23:07 +0100

    Add project files.

---
+++ DMEmpower-HelloWorld_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DMEmpower-HelloWorld_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("DMEmpower-HelloWorld_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("EB74C5D4-901C-41F5-9320-AF58C5D3A846")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,1 @@
+# DMEmpower-HelloWorld
